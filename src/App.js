cc01a19f3e932051afa8d67fe9cea293eca197ad
kimgun95 <54833128+kimgun95@users.noreply.github.com>
2021-08-13 19:45:47 +0900

    function을 다른것으로 변경 및 web3를 metamask에 연결

--- src/App.js
+++ src/App.js
@@ -1,14 +1,51 @@
-
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 import Web3 from 'web3';
 
-function App() {
 
-  
+class App extends Component {
 
+  async componentDidMount() {
+    await this.initWeb3();
+    console.log(this.web3);
+    let accounts = await this.web3.eth.getAccounts();
+    console.log(accounts);
+    let balance = await this.web3.eth.getBalance(accounts[0]);
+    console.log(balance);
+  }
+
+  // web3를 metamask에 연결
+  initWeb3 = async () => {
+    if (window.ethereum) {
+      console.log('Recent mode')
+      this.web3 = new Web3(window.ethereum);
+      try {
+          // Request account access if needed
+          await window.ethereum.enable();
+          // Acccounts now exposed
+          // this.web3.eth.sendTransaction({/* ... */});
+      } catch (error) {
+          // User denied account access...
+          console.log(`User denied account access error : ${error}`)
+      }
+    }
+    // Legacy dapp browsers...
+    else if (window.web3) {
+      console.log('legacy mode')
+      this.web3 = new Web3(Web3.currentProvider);
+      // Acccounts always exposed
+      // web3.eth.sendTransaction({/* ... */});
+    }
+    // Non-dapp browsers...
+    else {
+      console.log('Non-Ethereum browser detected. You should consider trying MetaMask!');
+    }
   
+  }
+
+  render() {
     return (
       <div className="App">
         <header className="App-header">
@@ -27,7 +64,7 @@ function App() {
         </header>
       </div>
     );
-  
+  }
 
 }
 
